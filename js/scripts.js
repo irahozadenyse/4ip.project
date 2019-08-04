8b28fab8504543b30789dfa5c984bdc9603a7032
irahozadenyse <irahodeny1@gmail.com>
2019-08-04 20:55:22 +0200

    added a function in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 
+//bussiness logic
 function Information(crust, topping,flavour,size) {
   this.crust = crust;
   this.topping = topping;
@@ -7,20 +8,20 @@ function Information(crust, topping,flavour,size) {
 }
 
  Information.prototype.fullInformation= function() {
-  return this.crust + " " + this.topping+" "+this.flavour+" "+this.size;
+  return this.crust + " " + this.topping+"  "+ this.flavour +"  "+ this.size;
  }
 
 
-function resetFields() {
-  $("select#sel1").val("");
-  $("select#sel2").val("");
-  $("select#sel3").val("");
-  $("select#sel4").val("");
+// function resetFields() {
+//   $("select#sel1").val("");
+//   $("select#sel2").val("");
+//   $("select#sel3").val("");
+//   $("select#sel4").val("");
   
-}
+// }
  
 
-
+//user iterface logic
 
 $(document).ready(function(){
   $("#piz").hide();
@@ -36,9 +37,6 @@ $(".img1").hover(function(){
 });
 
 
- 
-
-
 
   $("#add-menu").click(function() {
     $(".yes").append('<div class="form-group">'+
@@ -46,11 +44,11 @@ $(".img1").hover(function(){
               '<select class="control" id="sel1">'+
          
               '<option value="neapolitian">neapolitian  $3</option>'+
-         '<option value="new york style">new york style $3</option>'+
-         '<option value="chicago deep dish">chicago deep dish $4</option>'+
+         '<option value="new york style">newyorkstyle $3</option>'+
+         '<option value="chicago deep dish">chicagodeepdish $4</option>'+
          '<option value="flatbread/focaccia">flatbread/focaccia $2</option>'+
-         '<option value="gluten-free">gluten-free $3</option>'+
-         '<option value="vegan-friendly">vegan-friendly $4</option>'+
+         '<option value="gluten-free">glutenfree $3</option>'+
+         '<option value="vegan-friendly">veganfriendly $4</option>'+
        '</select>'+
      '</div>'+
 
@@ -62,8 +60,8 @@ $(".img1").hover(function(){
           '<option value="nuts">nuts    $1 </option>'+
           '<option value="meats">meats    $3</option>'+
           '<option value="cheese">cheese    $1.50</option>'+
-          '<option value="sea food">sea food    $2.00</option>'+
-          '<option value="Herbs and Spîces">Herbs and Spîces    $1.50 </option>'+
+          '<option value="sea food">seafood    $2.00</option>'+
+          '<option value="Herbs and Spîces">HerbsandSpîces    $1.50 </option>'+
         '</select>'+
       '</div>'+
 
@@ -71,11 +69,11 @@ $(".img1").hover(function(){
              
       '<label for="sel1">Select Flavours you want:</label><br>'+
       '<select class="control" id="sel1">'+
-           '<option value="Double Cheese Pizza">Double Cheese Pizza    $3</option>'+
+           '<option value="Double Cheese Pizza">DoubleCheesePizza    $3</option>'+
            '<option value="Gourmet"> Gourmet   $2</option>'+
-           '<option value="Mexican Green Wave">Mexican Green Wave    $1.5</option>'+
-           '<option value="Peppy Paneer"> Peppy Paneer    $1.5</option>'+
-           '<option value=" Margherita Pizza"> Margherita Pizza    $1.5</option>'+
+           '<option value="Mexican Green Wave">MexicanGreenWave    $1.5</option>'+
+           '<option value="Peppy Paneer"> PeppyPaneer    $1.5</option>'+
+           '<option value=" Margherita Pizza"> MargheritaPizza    $1.5</option>'+
          '</select>'+
        '</div>'+
 
@@ -86,14 +84,14 @@ $(".img1").hover(function(){
             '<option value="Small">Small    $1.5</option>'+
             '<option value="Medium">Medium    $3</option>'+
             '<option value="Large">Large    $4.5</option>'+
-            '<option value="Extra-large">Extra-large    $6</option>'+
+            '<option value="Extra-large">Extralarge    $6</option>'+
           '</select>'+
         '</div>')
  
   });
   $("form#fill").submit(function(event) {
     event.preventDefault();
-
+   j=0;
     var selectedCrust = $("select#sel1").val();
     console.log(selectedCrust);
     var selectedTopping=$("select#sel2").val();
@@ -103,11 +101,11 @@ $(".img1").hover(function(){
   console.log(newInformation);
 
 
-  $("ol#pizzas").append("<li><span" + newInformation.fullInformation() + "</span></li>");
+  $("ol#pizzas").append("<li><span>" + newInformation.fullInformation() + "</span></li>");
     
     $("#sub").last().click(function() {
       $("#show-check").show();
-      $("#show-check h3").text(newInformation.fullInformation());
+      $("#show-check h2").text(newInformation.fullInformation());
       $(".crusters-now").text(newInformation.crust);
       $(".topping-now").text(newInformation.topping);
       $(".flavour-now").text(newInformation.flavour);
@@ -120,13 +118,34 @@ $(".img1").hover(function(){
     $("select#sel3").val();
     $("select#sel4").val();
  
-  });
-
+  })
 })
-  
+
+if(newInformation.size=='extralarge'){
+  j=10;
+  $("span.price").text(j +'$')
+}
+else if(newInformation.size=='large'){
+  j=7;
+  $("span.price").text(j +'$')
+}
+else if(newInformation.size=='medium'){
+j=5;
+$("span.price").text(j +'$')
+}
+else if(newInformation.size=='small'){
+  j=3;
+  $("span.price").text(j +'$')
+}
 
 
 
+$("button#yes".click(function(){
+  prompt("enter your name and address");
+  alert("you are going to get it soon");
+})
+
+)
 
 
 
